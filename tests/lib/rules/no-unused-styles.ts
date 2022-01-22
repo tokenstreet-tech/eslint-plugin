626f695034215d20a0dc32ca2607578b3da8a9aa
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-22 16:14:30 +0100

    test: naming for text cases

--- tests/lib/rules/no-unused-styles.ts
+++ tests/lib/rules/no-unused-styles.ts
@@ -35,64 +35,75 @@ ruleTester.run('no-unused-styles', noUnusedStyles, {
                 'const styles = StyleSheet.create({ name: {} });\n',
         },
         {
-            name: 'Function component, declared after the styles',
+            name: 'Component declared after the styles',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = () => <Text style={styles.name}>Hello</Text>;\n',
         },
         {
+            name: 'Multiple style objects',
             code:
                 'const styles = StyleSheet.create({ name: {},  welcome: {} });\n' +
                 'const Hello = () => <Text style={styles.name}>Hello</Text>;\n' +
                 'const Welcome = () => <Text style={styles.welcome}>Welcome</Text>;\n',
         },
         {
+            name: 'Style object passed as a variable',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = ({ textStyle }) => <Text style={[styles.name, textStyle]}>Hello</Text>;\n',
         },
         {
+            name: 'Style objects from different style sheets',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const styles2 = StyleSheet.create({ name: {} });\n' +
                 'const Hello = () => <Text style={[styles.name, styles2.name]}>Hello</Text>;\n',
         },
         {
+            name: 'Conditional style objects (positive)',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = ({condition = true}) => <Text style={condition && styles.name}>Hello</Text>;\n',
         },
         {
+            name: 'Conditional style objects (negative)',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = ({condition = false}) => <Text style={condition && styles.name}>Hello</Text>;\n',
         },
         {
+            name: 'Conditional style objects inside an array (positive)',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = ({condition = true}) => <Text style={[condition && styles.name]}>Hello</Text>;\n',
         },
         {
+            name: 'Conditional style objects inside an array (negative)',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = ({condition = false}) => <Text style={[condition && styles.name]}>Hello</Text>;\n',
         },
         {
+            name: 'Ternary operator',
             code:
                 'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
                 'const Hello = ({ condition = true }) => <Text style={condition ? styles.name : styles.welcome}>Hello</Text>;\n',
         },
         {
+            name: 'Ternary operator inside an array',
             code:
                 'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
                 'const Hello = ({ condition = true }) => <Text style={[condition ? styles.name : styles.welcome]}>Hello</Text>;\n',
         },
         {
+            name: 'createClass syntax',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'const Hello = React.createClass({ render: () => <Text style={styles.name}>Hello</Text> });\n',
         },
         {
+            name: 'Class component syntax',
             code:
                 'const styles = StyleSheet.create({ name: {} });\n' +
                 'class Hello extends Component {\n' +
@@ -101,8 +112,19 @@ ruleTester.run('no-unused-styles', noUnusedStyles, {
                 '    }\n' +
                 '}\n',
         },
-        { code: 'const styles = StyleSheet.create({ name: {} });\n' },
         {
+            name: 'Pure component syntax',
+            code:
+                'const styles = StyleSheet.create({ name: {} });\n' +
+                'class Hello extends PureComponent {\n' +
+                '    render() {\n' +
+                '        return <Text style={styles.name}>Hello</Text>;\n' +
+                '    }\n' +
+                '}\n',
+        },
+        { name: 'Only the style sheet', code: 'const styles = StyleSheet.create({ name: {} });\n' },
+        {
+            name: 'Style object variable',
             code:
                 'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
                 'const Hello = ({ condition = true }) => {\n' +
@@ -111,12 +133,14 @@ ruleTester.run('no-unused-styles', noUnusedStyles, {
                 '};\n',
         },
         {
+            name: 'Spread syntax',
             code:
                 'const additionalStyles = {};\n' +
                 'const styles = StyleSheet.create({ name: {}, ...additionalStyles });\n' +
                 'const Hello = () => <Text style={styles.name}>Hello</Text>;\n',
         },
         {
+            name: 'Custom style sheet',
             code:
                 'const styles = EStyleSheet.create({ name: {} });\n' +
                 'const Hello = () => <Text style={styles.name}>Hello</Text>;\n',
