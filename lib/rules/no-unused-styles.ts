1560c75b3b7d4a270e2ff2c43a242bf658990995
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-22 13:16:19 +0100

    feat: first draft

---
+++ lib/rules/no-unused-styles.ts
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview Detects unused styles
+ * @author Daniel Reichhart <daniel@tokenstreet.com>
+ */
+
+import { StyleSheets } from '../util/StyleSheet';
+import Components from '../util/Components';
+import { astHelpers } from '../util/astHelpers';
+
+const create = Components.detect((context: any, components: any) => {
+    const styleSheets = new StyleSheets();
+    const styleReferences = new Set();
+
+    function reportUnusedStyles(unusedStyles: any) {
+        Object.keys(unusedStyles).forEach((key) => {
+            if ({}.hasOwnProperty.call(unusedStyles, key)) {
+                const styles = unusedStyles[key];
+                styles.forEach((node: any) => {
+                    const message = ['Unused style detected: ', key, '.', node.key.name].join('');
+
+                    context.report(node, message);
+                });
+            }
+        });
+    }
+
+    return {
+        MemberExpression: function (node: any) {
+            const styleRef = astHelpers.getPotentialStyleReferenceFromMemberExpression(node);
+            if (styleRef) {
+                styleReferences.add(styleRef);
+            }
+        },
+
+        CallExpression: function (node: any) {
+            if (astHelpers.isStyleSheetDeclaration(node, context.settings)) {
+                const styleSheetName = astHelpers.getStyleSheetName(node);
+                const styles = astHelpers.getStyleDeclarations(node);
+
+                styleSheets.add(styleSheetName, styles);
+            }
+        },
+
+        'Program:exit': function () {
+            const list = components.all();
+            if (Object.keys(list).length > 0) {
+                styleReferences.forEach((reference) => {
+                    styleSheets.markAsUsed(reference);
+                });
+                reportUnusedStyles(styleSheets.getUnusedReferences());
+            }
+        },
+    };
+});
+
+module.exports = {
+    meta: {
+        schema: [],
+    },
+    create,
+};
