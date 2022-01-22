6398b8996f2a630663f95f913a91e489edb78168
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-22 20:11:45 +0100

    simplification

--- lib/rules/no-unused-styles.ts
+++ lib/rules/no-unused-styles.ts
@@ -3,15 +3,52 @@
  * @author Daniel Reichhart <daniel@tokenstreet.com>
  */
 
-import { StyleSheets } from '../util/StyleSheet';
 import Components from '../util/Components';
-import {
-    getPotentialStyleReferenceFromMemberExpression,
-    isStyleSheetDeclaration,
-    getStyleSheetName,
-    getStyleDeclarations,
-} from '../util/astHelpers';
 import { Rule } from 'eslint';
+
+/**
+ * StyleSheets represents the StyleSheets found in the source code.
+ */
+export class StyleSheets {
+    private styleSheets: any = {};
+
+    /**
+     * Add adds a StyleSheet to our StyleSheets collections.
+     *
+     * @param {string} styleSheetName - The name of the StyleSheet.
+     * @param {object} properties - The collection of rules in the styleSheet.
+     */
+    add(styleSheetName: string, properties: any) {
+        this.styleSheets[styleSheetName] = properties;
+    }
+
+    /**
+     * MarkAsUsed marks a rule as used in our source code by removing it from the
+     * specified StyleSheet rules.
+     *
+     * @param {string} fullyQualifiedName - The fully qualified name of the rule.
+     * for example 'styles.text'
+     */
+    markAsUsed(fullyQualifiedName: string) {
+        const nameSplit = fullyQualifiedName.split('.');
+        const styleSheetName = nameSplit[0];
+        const styleSheetProperty = nameSplit[1];
+
+        if (this.styleSheets[styleSheetName]) {
+            this.styleSheets[styleSheetName] = this.styleSheets[styleSheetName].filter(
+                (property: any) => property.key.name !== styleSheetProperty
+            );
+        }
+    }
+
+    /**
+     * GetUnusedReferences returns all collected StyleSheets and their
+     * unmarked rules.
+     */
+    getUnusedReferences() {
+        return this.styleSheets;
+    }
+}
 
 export const noUnusedStyles: Rule.RuleModule = {
     meta: {
@@ -19,7 +56,7 @@ export const noUnusedStyles: Rule.RuleModule = {
     },
     create: Components.detect((context: Rule.RuleContext, components: any) => {
         const styleSheets = new StyleSheets();
-        const styleReferences = new Set();
+        const styleReferences = new Set<string>();
 
         function reportUnusedStyles(unusedStyles: any) {
             Object.keys(unusedStyles).forEach((key) => {
@@ -34,17 +71,72 @@ export const noUnusedStyles: Rule.RuleModule = {
             });
         }
 
+        const containsStyleSheetObject = (node: any, objectNames: any) =>
+            Boolean(
+                node &&
+                    node.type === 'CallExpression' &&
+                    node.callee &&
+                    node.callee.object &&
+                    node.callee.object.name &&
+                    objectNames.includes(node.callee.object.name)
+            );
+
+        const containsCreateCall = (node: any) =>
+            Boolean(node && node.callee && node.callee.property && node.callee.property.name === 'create');
+
+        const isStyleSheetDeclaration = (node: any, settings: any) => {
+            const getStyleSheetObjectNames = (settings: any) =>
+                settings['react-native/style-sheet-object-names'] || ['StyleSheet'];
+            const objectNames = getStyleSheetObjectNames(settings);
+
+            return Boolean(containsStyleSheetObject(node, objectNames) && containsCreateCall(node));
+        };
+
+        const getStyleSheetName = (node: any) => {
+            if (node && node.parent && node.parent.id) {
+                return node.parent.id.name;
+            }
+        };
+
+        const getStyleDeclarations = (node: any) => {
+            if (
+                node &&
+                node.type === 'CallExpression' &&
+                node.arguments &&
+                node.arguments[0] &&
+                node.arguments[0].properties
+            ) {
+                return node.arguments[0].properties.filter((property: any) => property.type === 'Property');
+            }
+
+            return [];
+        };
+
+        const getPotentialStyleReferenceFromMemberExpression = (node: any) => {
+            if (
+                node &&
+                node.object &&
+                node.object.type === 'Identifier' &&
+                node.object.name &&
+                node.property &&
+                node.property.type === 'Identifier' &&
+                node.property.name &&
+                node.parent.type !== 'MemberExpression'
+            ) {
+                return [node.object.name, node.property.name].join('.');
+            }
+        };
+
         return {
             // Style refs
-            MemberExpression: function (node: any) {
+            MemberExpression: (node: any) => {
                 const styleRef = getPotentialStyleReferenceFromMemberExpression(node);
                 if (styleRef) {
                     styleReferences.add(styleRef);
                 }
             },
-
             // Stylesheets
-            CallExpression: function (node: any) {
+            CallExpression: (node: any) => {
                 if (isStyleSheetDeclaration(node, context.settings)) {
                     const styleSheetName = getStyleSheetName(node);
                     const styles = getStyleDeclarations(node);
@@ -52,8 +144,7 @@ export const noUnusedStyles: Rule.RuleModule = {
                     styleSheets.add(styleSheetName, styles);
                 }
             },
-
-            'Program:exit': function () {
+            'Program:exit': () => {
                 const list = components.all();
                 if (Object.keys(list).length > 0) {
                     styleReferences.forEach((reference) => {
