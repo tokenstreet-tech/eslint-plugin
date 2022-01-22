79e90237cd98a66a25d70f81a99ebb178f33645b
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-22 18:13:48 +0100

    test: improvements

--- tests/lib/rules/no-unused-styles.ts
+++ tests/lib/rules/no-unused-styles.ts
@@ -196,47 +196,137 @@ ruleTester.run('no-unused-styles', noUnusedStyles, {
             errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
         {
-            name: 'Wrong style sheet',
+            name: 'Component declared after the styles',
             code:
-                'const styles = StyleSheet.create({ text: {} });\n' +
-                'const Hello = () => <Text style={styles.welcome}>Hello</Text>;',
-            errors: [{ message: 'Unused style detected: styles.text' }],
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = () => <Text style={styles.name}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Multiple style objects',
+            code:
+                'const styles = StyleSheet.create({ name: {},  welcome: {}, button: {} });\n' +
+                'const Hello = () => <Text style={styles.name}>Hello</Text>;\n' +
+                'const Welcome = () => <Text style={styles.welcome}>Welcome</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.button' }],
+        },
+        {
+            name: 'Style object passed as a variable',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = ({ textStyle }) => <Text style={[styles.name, textStyle]}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Style objects from different style sheets',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const styles2 = StyleSheet.create({ name: {} });\n' +
+                'const Hello = () => <Text style={[styles.name, styles2.name]}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Conditional style objects (positive)',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = ({condition = true}) => <Text style={condition && styles.name}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Conditional style objects (negative)',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = ({condition = false}) => <Text style={condition && styles.name}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Conditional style objects inside an array (positive)',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = ({condition = true}) => <Text style={[condition && styles.name]}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Conditional style objects inside an array (negative)',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = ({condition = false}) => <Text style={[condition && styles.name]}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Ternary operator',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {}, button: {} });\n' +
+                'const Hello = ({ condition = true }) => <Text style={condition ? styles.name : styles.welcome}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.button' }],
+        },
+        {
+            name: 'Ternary operator inside an array',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {}, button: {} });\n' +
+                'const Hello = ({ condition = true }) => <Text style={[condition ? styles.name : styles.welcome]}>Hello</Text>;  \n',
+            errors: [{ message: 'Unused style detected: styles.button' }],
+        },
+        {
+            name: 'createClass syntax',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
+                'const Hello = React.createClass({ render: () => <Text style={styles.name}>Hello</Text> }); \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
         {
             name: 'Class component syntax',
             code:
-                'const styles = StyleSheet.create({ text: {}, welcome: {} });\n' +
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
                 'class Hello extends Component {\n' +
                 '    render() {\n' +
-                '        return <Text style={styles.text}>Hello</Text>;\n' +
+                '        return <Text style={styles.name}>Hello</Text>;\n' +
                 '    }\n' +
-                '}',
+                '}  \n',
             errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
         {
             name: 'Pure component syntax',
             code:
-                'const styles = StyleSheet.create({ text: {}, welcome: {} });\n' +
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });\n' +
                 'class Hello extends PureComponent {\n' +
                 '    render() {\n' +
-                '        return <Text style={styles.text}>Hello</Text>;\n' +
+                '        return <Text style={styles.name}>Hello</Text>;\n' +
                 '    }\n' +
-                '}',
+                '}  \n',
             errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
         {
             name: 'Memoized function component',
             code:
-                'const styles = StyleSheet.create({ text: {}, welcome: {} });\n' +
-                'const Hello = memo(() => <Text style={styles.text}>Hello</Text>);\n' +
-                "Hello.displayName = 'Hello';\n",
+                'const Hello = memo(() => <Text style={styles.name}>Hello</Text>);\n' +
+                "Hello.displayName = 'Hello';\n" +
+                'const styles = StyleSheet.create({ name: {}, welcome: {} });  \n',
+            errors: [{ message: 'Unused style detected: styles.welcome' }],
+        },
+        {
+            name: 'Style object variable',
+            code:
+                'const styles = StyleSheet.create({ name: {}, welcome: {}, button: {} });\n' +
+                'const Hello = ({ condition = true }) => {\n' +
+                '    const myStyle = condition ? styles.name : styles.welcome;\n' +
+                '    return <Text style={myStyle}>Hello</Text>;\n' +
+                '};  \n',
+            errors: [{ message: 'Unused style detected: styles.button' }],
+        },
+        {
+            name: 'Spread syntax',
+            code:
+                'const additionalStyles = {};\n' +
+                'const styles = StyleSheet.create({ name: {}, welcome: {}, ...additionalStyles });\n' +
+                'const Hello = () => <Text style={styles.name}>Hello</Text>;  \n',
             errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
         {
             name: 'Custom style sheet',
             code:
                 'const styles = EStyleSheet.create({ name: {}, welcome: {} });\n' +
-                'const Hello = () => <Text style={styles.name}>Hello</Text>;\n',
+                'const Hello = () => <Text style={styles.name}>Hello</Text>;  \n',
             errors: [{ message: 'Unused style detected: styles.welcome' }],
         },
     ],
