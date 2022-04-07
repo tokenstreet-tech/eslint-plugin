03ffb605aebec4611f6639e662a05afe9bdcfa05
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-04-07 09:43:01 +0200

    better file description

--- tests/lib/rules/no-throw.ts
+++ tests/lib/rules/no-throw.ts
@@ -1,5 +1,8 @@
 /**
  * @fileoverview
+ * We use the ErrorHandler to handle warnings, errors and fatal errors,
+ * which reacts appropriately and unified to the severity.
+ * Therefore, this should always be used instead of throwing an error yourself.
  *
  * @author Daniel Reichhart <daniel@tokenstreet.com>
  */
