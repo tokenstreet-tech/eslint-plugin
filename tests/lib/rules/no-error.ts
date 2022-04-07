ebb372165184dca28b24779eaa71e3e13b740a2e
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2022-04-07 09:45:05 +0200

    Better error rules description (#100)
    * first draft
    
    * fix
    
    * fix
    
    * final
    
    * fix
    
    * more test cases
    
    * better file description

--- tests/lib/rules/no-error.ts
+++ tests/lib/rules/no-error.ts
@@ -1,5 +1,8 @@
 /**
  * @fileoverview
+ * We use the ErrorHandler to handle warnings, errors and fatal errors,
+ * which reacts appropriately and unified to the severity.
+ * Therefore, this should always be used instead of throwing an error yourself.
  *
  * @author Daniel Reichhart <daniel@tokenstreet.com>
  */
@@ -37,5 +40,13 @@ ruleTester.run('no-error', noError, {
         { code: "new SyntaxError('foo');", errors: [error] },
         { code: "new TypeError('foo');", errors: [error] },
         { code: "new URIError('foo');", errors: [error] },
+        { code: "throw new Error('foo');", errors: [error] },
+        { code: "throw new EvalError('foo');", errors: [error] },
+        { code: "throw new InternalError('foo');", errors: [error] },
+        { code: "throw new RangeError('foo');", errors: [error] },
+        { code: "throw new ReferenceError('foo');", errors: [error] },
+        { code: "throw new SyntaxError('foo');", errors: [error] },
+        { code: "throw new TypeError('foo');", errors: [error] },
+        { code: "throw new URIError('foo');", errors: [error] },
     ],
 });
